67aa7c6a5e63c4a1d9f55939caaedfad5d49ff66
shaibazkhan <sknizami@gmail.com>
2019-02-01 15:34:30 +0000

    fixed failing e2e test

--- src/main/java/uk/gov/hmcts/reform/finrem/documentgenerator/model/DocumentRequest.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/documentgenerator/model/DocumentRequest.java
@@ -15,7 +15,7 @@ public class DocumentRequest {
     @JsonProperty(value = "template", required = true)
     @NotBlank
     private final String template;
-    @ApiModelProperty(value = "Name of the file", required = false)
+    @ApiModelProperty(value = "Name of the file")
     @JsonProperty(value = "fileName", required = true)
     private final String fileName;
     @JsonProperty(value = "values", required = true)
