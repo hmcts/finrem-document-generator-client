edd41cd70c6f61ffb17d6bdb1e03329b78a82263
shaibazkhan <sknizami@gmail.com>
2019-02-04 10:36:56 +0000

    added test for @Value

--- src/test/java/uk/gov/hmcts/reform/finrem/documentgenerator/model/DocumentRequestTest.java
+++ src/test/java/uk/gov/hmcts/reform/finrem/documentgenerator/model/DocumentRequestTest.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.reform.finrem.documentgenerator.model;
 import com.google.common.collect.ImmutableMap;
 import org.junit.Test;
 
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.isEmptyString;
 import static org.junit.Assert.assertThat;
@@ -13,9 +14,20 @@ public class DocumentRequestTest {
 
     @Test
     public void properties() {
-        DocumentRequest request = new DocumentRequest(TEMPLATE, "", ImmutableMap.of());
+        DocumentRequest request = documentRequest();
         assertThat(request.getTemplate(), is(TEMPLATE));
         assertThat(request.getValues(), is(ImmutableMap.of()));
         assertThat(request.getFileName(), isEmptyString());
     }
+
+    @Test
+    public void valueTest() {
+        assertThat(documentRequest(), is(equalTo(documentRequest())));
+    }
+
+    private DocumentRequest documentRequest() {
+        return new DocumentRequest(TEMPLATE, "", ImmutableMap.of());
+    }
+
+
 }
