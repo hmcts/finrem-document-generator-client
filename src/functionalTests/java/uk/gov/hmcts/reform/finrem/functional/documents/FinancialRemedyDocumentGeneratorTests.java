27ed98f06265d69d580fb22853dc22a190f7a6a9
shaibazkhan <sknizami@gmail.com>
2019-03-22 19:39:17 +0000

    added sop

--- src/functionalTests/java/uk/gov/hmcts/reform/finrem/functional/documents/FinancialRemedyDocumentGeneratorTests.java
+++ src/functionalTests/java/uk/gov/hmcts/reform/finrem/functional/documents/FinancialRemedyDocumentGeneratorTests.java
@@ -93,6 +93,7 @@ public class FinancialRemedyDocumentGeneratorTests extends IntegrationTestBase {
             .body(utils.getJsonFromFile(jsonFileName))
             .when().post().andReturn();
 
+        System.out.println("response received---->" + jsonResponse.prettyPrint());
         return jsonResponse;
     }
 
