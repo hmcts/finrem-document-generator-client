8509d01a421984be96eefa9935eca7211fdcde72
Chandra Korivi <Chandra.Korivi@HMCTS.NET>
2019-07-09 11:28:31 +0100

    FR-1021 stamping of pdf, work in progress.

--- src/main/java/uk/gov/hmcts/reform/finrem/documentgenerator/service/PDFStampingService.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/documentgenerator/service/PDFStampingService.java
@@ -1,33 +1,59 @@
 package uk.gov.hmcts.reform.finrem.documentgenerator.service;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
-import uk.gov.hmcts.reform.finrem.documentgenerator.GeneratedDocumentInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import uk.gov.hmcts.reform.finrem.documentgenerator.config.PdfDocumentConfig;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.Map;
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 
 public class PDFStampingService {
 
-    public static byte[] stampDocument(Map<String, GeneratedDocumentInfo> generatedDocumentInfoList) throws IOException {
-        PDDocument doc = PDDocument.load(generatedDocumentInfoList.get(0).getBytes());
+    @Autowired
+    private PdfDocumentConfig config;
 
-        //get the first page of the document
+    public byte[] stampDocument(byte[] inputFileInBytes, byte[] courtSealImage) throws Exception {
+
+        //retrieve doc from DM store
+        ByteArrayOutputStream outputBytes = new ByteArrayOutputStream();
+
+        PDDocument doc = PDDocument.load(inputFileInBytes);
+        //Retrieving the page
         PDPage page = doc.getPage(0);
 
-        //retrieve stamp from assets
-        PDImageXObject pdImage = PDImageXObject.createFromFile("finrem-document-generator-client/docs/courtSeal.PNG", doc);
+        PDImageXObject pdImage = PDImageXObject.createFromByteArray(doc, courtSealImage, null);
 
         PDPageContentStream contentStream = new PDPageContentStream(doc, page, PDPageContentStream.AppendMode.APPEND,
             true, true);
         contentStream.drawImage(pdImage, 100, 500, 170, 175);
         contentStream.close();
+
+        doc.save(outputBytes);
         doc.close();
 
         //return document that has been stamped as byte[]
-        return new byte[0];
+        return outputBytes.toByteArray();
+    }
+
+    public byte[] getCourSeal() throws Exception {
+        try (InputStream inputStream = getClass().getResourceAsStream("/courtseal.png")) {
+            return IOUtils.toByteArray(inputStream);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        PDFStampingService service = new PDFStampingService();
+        FileInputStream inputFile = new FileInputStream("/Users/chandrashekharkorivi/Downloads/NCB_Test.pdf");
+        byte[] courSeal = service.getCourSeal();
+        byte[] bytes = service.stampDocument(IOUtils.toByteArray(inputFile), courSeal);
+        FileOutputStream fos = new FileOutputStream("/Users/chandrashekharkorivi/Downloads/NCB_Test_Stamped.pdf");
+        fos.write(bytes);
+        fos.close();
     }
 }
