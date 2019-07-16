9448d23f570ddb0a151c9e6a04de2717ad0049f0
Hasan Shaik <khaja.shaikh@hmcts.net>
2019-07-16 15:36:22 +0100

    SendLetterAutoConfiguration.class in exclude list

--- src/main/java/uk/gov/hmcts/reform/finrem/documentgenerator/DocumentGeneratorApplication.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/documentgenerator/DocumentGeneratorApplication.java
@@ -3,9 +3,11 @@ package uk.gov.hmcts.reform.finrem.documentgenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import uk.gov.hmcts.reform.authorisation.healthcheck.ServiceAuthHealthIndicator;
+import uk.gov.hmcts.reform.sendletter.SendLetterAutoConfiguration;
 
 @EnableFeignClients (basePackages = {"uk.gov.hmcts.reform.sendletter"})
-@SpringBootApplication
+@SpringBootApplication(exclude = {SendLetterAutoConfiguration.class})
 public class DocumentGeneratorApplication {
 
     public static void main(String[] args) {
