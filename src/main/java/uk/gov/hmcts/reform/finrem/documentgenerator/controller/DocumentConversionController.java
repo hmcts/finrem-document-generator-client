4880989cccfb41ab393246d31e6587a3f6649980
Owen Carleton <owenc@kainos.com>
2020-11-03 15:27:36 +0000

    adding pdf conversion services and controllers

---
+++ src/main/java/uk/gov/hmcts/reform/finrem/documentgenerator/controller/DocumentConversionController.java
@@ -0,0 +1,46 @@
+package uk.gov.hmcts.reform.finrem.documentgenerator.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+import uk.gov.hmcts.reform.finrem.documentgenerator.model.Document;
+import uk.gov.hmcts.reform.finrem.documentgenerator.service.DocumentConversionService;
+import uk.gov.hmcts.reform.finrem.documentgenerator.service.DocumentManagementService;
+
+@RestController
+@RequiredArgsConstructor
+@Api(value = "Document Generation", tags = {"Document Generation"})
+@Slf4j
+public class DocumentConversionController {
+
+    private DocumentConversionService documentConversionService;
+    private DocumentManagementService documentManagementService;
+
+    @ApiOperation(value = "Deletes document from the evidence management.", tags = {"Document Generation"})
+    @ApiResponses({
+        @ApiResponse(code = 204, message = "Document was deleted successfully and stored in the"
+            + " evidence management. Returns the url to the stored document.", response = String.class),
+        @ApiResponse(code = 400, message = "Returned when input parameters are invalid", response = String.class),
+        @ApiResponse(code = 503, message = "Returned Evidence Management Client Api "
+            + "cannot be reached", response = String.class),
+        @ApiResponse(code = 500, message = "Returned when there is an unknown server error",
+            response = String.class)
+    })
+    public Document convertDocumentToPdf(
+        @RequestHeader(value = "Authorization") String authorisationToken,
+        @RequestBody Document document
+    ) {
+        return storeDocument(documentConversionService.convertDocumentToPdf(document),
+            documentConversionService.getConvertedFilename(document.getFileName()), authorisationToken);
+    }
+
+    private Document storeDocument(byte[] source, String filename, String authorisationToken) {
+        return documentManagementService.storeDocument(source, filename, authorisationToken);
+    }
+}
---
+++ src/main/java/uk/gov/hmcts/reform/finrem/documentgenerator/service/DocumentConversionService.java
@@ -0,0 +1,103 @@
+package uk.gov.hmcts.reform.finrem.documentgenerator.service;
+
+
+
+
+import com.google.common.io.Files;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+import uk.gov.hmcts.reform.finrem.documentgenerator.error.DocumentConversionException;
+import uk.gov.hmcts.reform.finrem.documentgenerator.model.Document;
+
+import java.io.File;
+import java.io.IOException;
+
+@Service
+@Slf4j
+public class DocumentConversionService {
+
+    private final String docmosisAccessKey;
+    private final RestTemplate restTemplate;
+    private final String documentConversionUrl;
+    private static final String PDF = "pdf";
+    private final EvidenceManagementService evidenceManagementService;
+
+    public DocumentConversionService(
+        @Value("${service.pdf-service.accessKey}") String docmosisAccessKey,
+        @Value("${service.pdf-service.uri}/rs/convert") String documentConversionUrl,
+        RestTemplate restTemplate,
+        EvidenceManagementService evidenceManagementService
+    ) {
+        this.docmosisAccessKey = docmosisAccessKey;
+        this.restTemplate = restTemplate;
+        this.documentConversionUrl = documentConversionUrl;
+        this.evidenceManagementService = evidenceManagementService;
+    }
+
+    public byte[] convertDocumentToPdf(Document sourceDocument) {
+        if (sourceDocument.getFileName().toLowerCase().endsWith(PDF)) {
+            throw new DocumentConversionException(
+                "Document already is a pdf",
+                null
+            );
+        }
+
+        return convert(sourceDocument, PDF);
+    }
+
+    public String getConvertedFilename(String filename) {
+        return filename.split("\\.")[0] + ".pdf";
+    }
+
+    private byte[] convert(Document sourceDocument, String targetFileType) {
+        try {
+            String filename = getConvertedFilename(sourceDocument.getFileName());
+            byte[] docInBytes = evidenceManagementService.downloadDocument(sourceDocument.getBinaryUrl()).getBody();
+            File file = new File(filename);
+            Files.write(docInBytes, file);
+
+            return restTemplate
+                .postForObject(
+                    documentConversionUrl,
+                    createRequest(file, filename),
+                    byte[].class
+                );
+
+        } catch (HttpClientErrorException clientEx) {
+
+            throw new DocumentConversionException(
+                "Error converting document to pdf",
+                clientEx
+            );
+        } catch (IOException ex) {
+            throw new DocumentConversionException(
+                "Error creating temp file",
+                ex
+            );
+        }
+    }
+
+    private HttpEntity<MultiValueMap<String, Object>> createRequest(
+        File file,
+        String outputFilename
+    ) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+        body.add("accessKey", docmosisAccessKey);
+        body.add("outputName", outputFilename);
+        body.add("file", new FileSystemResource(file));
+
+        return new HttpEntity<>(body, headers);
+    }
+}
